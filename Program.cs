cf66ec9883e64aa2e02fcf00e8bfc5c691c8d756
aishanoor1439 <aisha.noor1439@gmail.com>
2025-03-22 10:27:23 +0100

    Final Commit
    completed

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Search
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 30;
+            int[] a = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+            int n = a.Length;
+            int l = 0;
+            int r = n - 1;
+            int mid;
+
+            while (l <= r)
+            {
+                mid = (l + r) / 2;
+
+                if (x == a[mid])
+                {
+                    Console.WriteLine(mid);
+                    return;
+                }
+                else if (x < a[mid])
+                {
+                    r = mid - 1;
+                }
+                else
+                {
+                    l = mid + 1;
+                }
+            }
+
+            Console.WriteLine(-1);
+        }
+    }
+}
+    
